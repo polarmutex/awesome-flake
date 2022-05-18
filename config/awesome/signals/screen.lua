6329a31bb3fb7bae9218294b26acd10fc8e670bc
Brian Ryall <brian@brianryall.xyz>
2022-05-18 13:59:06 -0400

    feat: auto switch wallpaper every 30 min

--- config/awesome/signals/screen.lua
+++ config/awesome/signals/screen.lua
@@ -4,15 +4,21 @@ local wibox = require('wibox')
 
 local vars = require('config.vars')
 local widgets = require('widgets')
+local gears = require('gears')
 
 screen.connect_signal('request::wallpaper', function(s)
     awful.wallpaper({
         screen = s,
         widget = {
             {
-                image = beautiful.wallpaper,
+                image = gears.filesystem.get_random_file_from_dir(
+                    os.getenv("HOME") .. '/.config/wallpapers',
+                    { '.jpg', '.png', '.svg' },
+                    true
+                ),
                 upscale = true,
                 downscale = true,
+                --resize = true,
                 widget = wibox.widget.imagebox,
             },
             valign = 'center',
@@ -22,6 +28,16 @@ screen.connect_signal('request::wallpaper', function(s)
         },
     })
 end)
+
+gears.timer({
+    timeout = 1800,
+    autostart = true,
+    callback = function()
+        for s in screen do
+            s:emit_signal('request::wallpaper')
+        end
+    end,
+})
 
 screen.connect_signal('request::desktop_decoration', function(s)
     awful.tag(vars.tags, s, awful.layout.layouts[1])
