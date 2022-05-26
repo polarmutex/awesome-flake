1fe7fbaa86aba73d4a52dbfd752ecee4f31133bb
Brian Ryall <brian@brianryall.xyz>
2022-05-26 12:55:43 -0400

    refactor: misc

--- config/awesome/rc.lua
+++ config/awesome/rc.lua
@@ -2,9 +2,6 @@
 -- found (e.g. lgi). If LuaRocks is not installed, do nothing.
 pcall(require, 'luarocks.loader')
 
--- Standard awesome library
-local gfs = require('gears.filesystem')
-local awful = require('awful')
 require('awful.autofocus')
 
 -- Notification library
@@ -22,8 +19,8 @@ end)
 
 -- load theme
 local beautiful = require('beautiful')
-local gears = require('gears')
-beautiful.init(gears.filesystem.get_themes_dir() .. 'default/theme.lua')
+local gfs = require('gears.filesystem')
+beautiful.init(gfs.get_themes_dir() .. 'default/theme.lua')
 
 -- load key and mouse bindings
 require('bindings')
