4fe10621df0b852bec9c9a886f8c9eec6edcdb23
Brian Ryall <brian@brianryall.xyz>
2022-05-26 14:36:12 -0400

    refactor: remove unused wallpaper

--- config/awesome/signals/screen.lua
+++ config/awesome/signals/screen.lua
@@ -1,43 +1,7 @@
 local awful = require('awful')
-local beautiful = require('beautiful')
-local wibox = require('wibox')
 
 local vars = require('configuration.vars')
 local bar = require('layouts.bar')
-local gears = require('gears')
-
-screen.connect_signal('request::wallpaper', function(s)
-    awful.wallpaper({
-        screen = s,
-        widget = {
-            {
-                image = gears.filesystem.get_random_file_from_dir(
-                    os.getenv('HOME') .. '/.config/wallpapers',
-                    { '.jpg', '.png', '.svg' },
-                    true
-                ),
-                upscale = true,
-                downscale = true,
-                --resize = true,
-                widget = wibox.widget.imagebox,
-            },
-            valign = 'center',
-            halign = 'center',
-            tiled = false,
-            widget = wibox.container.tile,
-        },
-    })
-end)
-
-gears.timer({
-    timeout = 1800,
-    autostart = true,
-    callback = function()
-        for s in screen do
-            s:emit_signal('request::wallpaper')
-        end
-    end,
-})
 
 --TODO compare to connect_for_each_screen
 screen.connect_signal('request::desktop_decoration', function(s)
