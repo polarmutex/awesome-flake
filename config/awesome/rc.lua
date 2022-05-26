6fd77b4f33dfdf00c18865c357a8a4f8c38a48a0
Brian Ryall <brian@brianryall.xyz>
2022-05-26 14:38:39 -0400

    refactor: rest of signals

--- config/awesome/rc.lua
+++ config/awesome/rc.lua
@@ -27,8 +27,21 @@ require('configuration')
 -- load key and mouse bindings
 require('bindings')
 
+--TODO compare to connect_for_each_screen
+local awful = require('awful')
+local bar = require('layouts.bar')
+local vars = require('configuration.vars')
+screen.connect_signal('request::desktop_decoration', function(s)
+    awful.tag(vars.tags, s, awful.layout.layouts[1])
+    s.promptbox = bar.create_promptbox()
+    s.layoutbox = bar.create_layoutbox(s)
+    s.taglist = bar.create_taglist(s)
+    s.tasklist = bar.create_tasklist(s)
+    s.wibox = bar.create_wibox(s)
+end)
+
 -- load signals
-require('signals')
+--require('signals')
 
 -- load theme
 require('theme')
