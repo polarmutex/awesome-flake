4af57acdbfe4fbfee641a6c140170a63e2214cc6
Brian Ryall <brian@brianryall.xyz>
2023-05-21 10:55:12 -0400

    fix: disable wallpapers in awesome

--- dotfiles/configuration/desktop.lua
+++ dotfiles/configuration/desktop.lua
@@ -42,38 +42,38 @@ end)
 
 --- Wallpapers
 --- ~~~~~~~~~-
-screen.connect_signal('request::wallpaper', function(s)
-    awful.wallpaper({
-        screen = s,
-        widget = {
-            {
-                image = gears.filesystem.get_random_file_from_dir(
-                    os.getenv('HOME') .. '/.config/wallpapers',
-                    { '.jpg', '.png', '.svg' },
-                    true
-                ),
-                upscale = true,
-                downscale = true,
-                --resize = true,
-                widget = wibox.widget.imagebox,
-            },
-            valign = 'center',
-            halign = 'center',
-            tiled = false,
-            widget = wibox.container.tile,
-        },
-    })
-end)
+--screen.connect_signal('request::wallpaper', function(s)
+--    awful.wallpaper({
+--        screen = s,
+--        widget = {
+--            {
+--                image = gears.filesystem.get_random_file_from_dir(
+--                    os.getenv('HOME') .. '/.config/wallpapers',
+--                    { '.jpg', '.png', '.svg' },
+--                    true
+--                ),
+--                upscale = true,
+--                downscale = true,
+--                --resize = true,
+--                widget = wibox.widget.imagebox,
+--            },
+--            valign = 'center',
+--            halign = 'center',
+--            tiled = false,
+--            widget = wibox.container.tile,
+--        },
+--    })
+--end)
 
-gears.timer({
-    timeout = 1800,
-    autostart = true,
-    callback = function()
-        for s in screen do
-            s:emit_signal('request::wallpaper')
-        end
-    end,
-})
+--gears.timer({
+--    timeout = 1800,
+--    autostart = true,
+--    callback = function()
+--        for s in screen do
+--            s:emit_signal('request::wallpaper')
+--        end
+--    end,
+--})
 
 --- Flash focus
 --- ~~~~~~~~~~~
